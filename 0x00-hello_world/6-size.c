ea44e47c5997befe030fb9d6b7ca5ff5f8b7fac1
Seki003 <mohamed-amine.nasik@uir.ac.ma>
2023-07-13 15:31:23 -0700

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main -6 6-size.c
+* with proper grammar, but the outcome is a piece of art,
+* Return: 0
+*/
+int main(void)
+{
+	printf("Size of a char: %zu\n", sizeof(char));
+	printf("Size of an int: %zu\n", sizeof(int));
+	printf("Size of a long int: %zu\n", sizeof(long int));
+	printf("Size of a long long int: %zu\n", sizeof(long long int));
+	printf("Size of a float: %zu\n", sizeof(float));
+	return (0);
