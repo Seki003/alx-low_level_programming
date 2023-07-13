b2f020536d60083bb63c88906dd7471f1f14f97c
Seki003 <mohamed-amine.nasik@uir.ac.ma>
2023-07-13 15:44:09 -0700

    update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main -101 101-quote.c
+* with proper grammar, but the outcome is a piece of art,
+* Return: 0
+*/
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
