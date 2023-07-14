0110abc34d2c5a45e4d7e0e8c3569b619b3dbd48
Seki <89064439+Seki003@users.noreply.github.com>
2023-07-14 23:24:48 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point of the program
  *
- * Description: Generates a random number and prints the last digit of it whith whether it's positive,
-
- *              greated than 5, or smaller.
+ * Description: Generates a random number and prints the last digit of it,
+ *              along with whether it's greater than 5 or smaller.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int n;
+    int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if (n % 10 > 5)
-        {
-                printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-        }
-        else if (n % 10 == 0)
-        {
-                printf("Last digit of %d is %d and is 0\n", n, n % 10);
-        }
-        else
-        {
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
-        }
-        return (0);
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    if (n % 10 > 5)
+    {
+        printf("Last digit of %d is %u and is greater than 5\n", n, n % 10);
+    }
+    else if (n % 10 == 0)
+    {
+        printf("Last digit of %d is %u and is 0\n", n, n % 10);
+    }
+    else
+    {
+        printf("Last digit of %d is %u and is less than 6 and not 0\n", n, n % 10);
+    }
+    return (0);
 }
