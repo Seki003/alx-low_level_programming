9a14b7d0bc51ca5be4b751eac2855a35ba1dc1e1
Seki003 <mohamed-amine.nassik@uir.ac.ma>
2023-07-19 00:48:59 +0100

    test

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * check if it is capitelized alphabet return 1,
+ * else return 0
+ */
+int _islower(int c);
+{
+	if (c >= 97 && c <=122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
