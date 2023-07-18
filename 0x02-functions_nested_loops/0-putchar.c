5d31b35a501859dcaca9e6761e3c6604d9fe654f
Seki003 <mohamed-amine.nassik@uir.ac.ma>
2023-07-18 23:31:14 +0100

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * the code is printing _putchar
- *
+ * Description: printing _putchar by using the function _putchar that use as an argument each carater of the text and combinet in one word
  * Return: Always 0.
  */
 int main(void)
