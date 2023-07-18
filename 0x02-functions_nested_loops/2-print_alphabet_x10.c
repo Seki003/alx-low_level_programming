6f431c05be8e51948157578ecee97ce92f9daebf
Seki003 <mohamed-amine.nassik@uir.ac.ma>
2023-07-19 00:30:16 +0100

    test

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * 
+ * 
+ * 
+*/
+
+void print_alphabet_x10(void)
+{
+    int i = 0;
+
+    char c = 'a';
+
+    while (i, i>=10, i++)
+    {
+        while (c>='z')
+        {
+            _putchar(c);
+        }
+        _putchar('\n');
+    }
+
+}
