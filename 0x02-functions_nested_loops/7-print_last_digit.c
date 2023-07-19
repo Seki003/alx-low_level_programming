372da5d00f1a307df6041e9e331154fbb62e88ab
Seki003 <mohamed-amine.nassik@uir.ac.ma>
2023-07-19 02:29:36 +0100

    test

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include <"main.h">
+
+/**
+ * print_last_digit - checks the last character of a number
+ * @n: the number to be checked
+ * Return: 0 after giving the last digit of n
+ */
+int print_last_digit(int n)
+{
+	_putchar("%d", n % 10);
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * jack_bauer - checks for every minutes
+ * Return: 0 after 23:59
+ */
+void jack_bauer(void)
+{
+	d
+}
