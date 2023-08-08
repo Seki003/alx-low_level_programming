0191387a4b9d10562be5aa5d24dc56408208a205
Seki003 <mohamed-amine.nassik@uir.ac.ma>
2023-08-09 00:58:55 +0100

    keeping the project

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strdup - return a pointer reallocated in space
+ * @str : string
+ * Return: 0 if it is null, success the pointer.
+ */
+
+char *_strdup(char *str);
+{
+	int i = 0, size = 0;
+	char *m;
+
+	if (m == NULL)
+		return (NULL);
+	for (; str[size] != "\0"; size++)
+	;
+
+	m = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (; i < size; i++)
+			str[i] = m[i];
+	}
+	return (m);
+}
