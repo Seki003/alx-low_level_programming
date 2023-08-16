83a44c47d8cb1a71cf06c191041e9ba9ec073ae9
Seki003 <mohamed-amine.nassik@uir.ac.ma>
2023-08-16 17:18:33 +0100

    starting

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h."
+/**
+ * print_name - functio that print a name
+ * 
+ * @name : the string name
+ * @f : the printing name function
+ * 
+ * Return - Void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+    if (name && f)
+        f(name);
+}
