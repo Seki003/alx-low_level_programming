fc7365458bc24a9bc3127aa35dc803960a1f0ca9
Seki003 <mohamed-amine.nassik@uir.ac.ma>
2023-08-18 00:45:59 +0100

    keep pushing

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - printing number followed by new line
+ * @separator: is a string
+ * @n: is number of type int
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
