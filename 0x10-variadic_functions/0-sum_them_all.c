99f637d838e57576555ab08353e8bf689a1a9e29
Seki003 <mohamed-amine.nassik@uir.ac.ma>
2023-08-18 00:28:59 +0100

    starting

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,16 +16,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	int i;
 
-	if (!n)
+	if (n == 0)
 		return (0);
 
 	for (i = 0; i < n; i++)
-	{
-		int x = va_arg(arg, int);
-
-		sum += z;
-	}
+		sum += va_arg(arg, int);
 
 	va_end(arg);
+
 	return (sum);
 }
