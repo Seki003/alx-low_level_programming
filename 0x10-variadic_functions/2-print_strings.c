73ce523c24ade1dd31e3fd66062e5bf977f234da
Seki003 <mohamed-amine.nassik@uir.ac.ma>
2023-08-18 01:12:05 +0100

    keep pushing

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - printing a string followed by new line
+ * @separator: character to separate
+ * @n: number of type int
+ * Return: void
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list ap;
+
+	va_start(ap, n)
+
+	if (separator == NULL)
+		separator = "";
+
+	for (n = 0; i < n; i++)
+	{
+		str = va_arg(ap, char*);
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s", str);
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
